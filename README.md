708110aa7f010a6d8e0a121a217fc611adebe4db
Mark Burggraf <markb@retina.local>
2014-04-05 13:43:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# icomoonlib
+
+## Titanium Alloy library for IcoMoon fonts
+ 	
+### setup
+1. create a font using the IcoMoon App located at http://icomoon.io
+2. download your font
+3. copy your <fontname>.ttf file to /app/assets/fonts
+4. copy your selection.json file to /app/assets/fontmaps and name it <fontname>.json
+5. copy this file (icomoonlib.js) to /app/lib
+6. call getIconXXXXX() functions from your app
+
+### common parameters:
+	
+	fontname: this is the name of the font file matching <fontname>.ttf and <fontname>.json
+
+	iconname: this is the name of the icon you want to retrieve
+
+	size: the size of the icon you want in dp (it'll be square by default)
+
+	options: an optional object of Titanium.UI.label properties to be applied to the object that is returned
+
+## methods
+
+### getIcon(fontname, iconname, size, options)
+Returns a local filename (including complete local path) of a png image file created to your specifications.  If the file does not exist, it will be created and saved to the Ti.Filesystem.applicationCacheDirectory folder.  If the file already exists in the cache, the complete path and filename will be returned.
+
+	var icomoonlib = require("icomoonlib");
+	// create a 32dp x 32dp red dog icon
+	var myicon = icon.getIcon("icomoon","big_red_dog",32,{color:"red"});
+	console.log("The cached image now resides at: " + myicon);
+	var imageView = Ti.UI.createImageView({image:myicon}); 
+
+### getIconAsLabel(fontname, iconname, size, options)
+Returns a Titanium.UI.Label object.
+
+	var icomoonlib = require("icomoonlib");
+	// create a 32dp x 32dp red dog icon
+	var mylabel = icon.getIconAsLabel("icomoon",
+										"big_red_dog",
+										32,
+										{color:"red",top:"10dp",left:"10dp"});
+	// mylabel contains a label object that can be added to a view or window
+
+### getIconAsBlob(fontname, iconname, size, options)
+Returns a Blob object containing an image.  The image is not cached, so getIcon is preferred in most cases, since it caches images.
+
+	var icomoonlib = require("icomoonlib");
+	// create a 32dp x 32dp red dog icon
+	var myicon = icon.getIconAsBlob("icomoon","big_red_dog",32,{color:"red"});
+	// myicon now holds a blob containing the image
+	var imageView = Ti.UI.createImageView({image:myicon}); 
+	
+### getIconAsImageView(fontname, iconname, size, options)
+Creates an image file using getIcon(), then creates and returns a Ti.UI.ImageView object containing the cached image.
+
+	var icomoonlib = require("icomoonlib");
+	// create a 32dp x 32dp red dog icon
+	var myimageview = icon.getIconAsImageView("icomoon","big_red_dog",32,{color:"red"});
+	// myimageview is a Ti.UI.ImageView object that can be added to a view or window
+
+### getFontList(fontname, size, options)
+For debug purposes only.  Opens a modal window showing the list of icons and icon names for the given font.  This is provided only for developers who have misplaced their icon name list and need to quickly find the name of an icon they need.
+
+	var icomoonlib = require("icomoonlib");
+	icomoonlib.getFontList("icomoon",32,{color:"blue");
---
+++ icomoonlib.js
@@ -0,0 +1,201 @@
+/* 
+ (This library is designed for use with Titanium Alloy)
+
+ getIcon - Get an icon from a saved IcoMoon font file as a Titanium Label object
+ 
+ setup:	
+ 1. create a font using the IcoMoon App located at http://icomoon.io
+ 2. download your font
+ 3. copy your <fontname>.ttf file to /app/assets/fonts
+ 4. copy your selection.json file to /app/assets/fontmaps and name it <fontname>.json
+ 5. copy this file (icomoonlib.js) to /app/lib
+ 5. call getIconXXXXX() from your app
+
+ usage:	getIcon(fontname, iconname, size, options)
+
+ fontname: this is the name of the font file matching <fontname>.ttf and <fontname>.json
+ iconname: this is the name of the icon you want to retrieve
+ size: the size of the icon you want in dp (it'll be square by default)
+ options: an optional object of Ti.UI.Label properties to apply to the resulting object
+
+ example:
+
+ var icon = require("icomoonlib"); // this file (icon.js), which should be copied to your /app/lib folder
+ var win = Ti.UI.createWindow({});
+ var myicon = icon.getIconAsImageView("icomoon","big_red_dog",32,{color:red}); // returns a 32dp x 32dp red dog icon
+ win.add(myicon);
+ */
+var fontMaps = {};
+
+function initializeFont(fontname) {
+	if ( typeof fontMaps[fontname] == "undefined")
+		fontMaps[fontname] = {};
+	try {
+		var obj = JSON.parse(Titanium.Filesystem.getFile('fontmaps/' + fontname + '.json').read().text);
+		var fontmap = obj.icons;
+		for (var i = 0; i < fontmap.length; i++) {
+			var font = fontmap[i].properties.name;
+			var code = fontmap[i].properties.code;
+			//console.log("Found: " + fontname + " / " + font + " / " + code);
+			fontMaps[fontname][font] = String.fromCharCode(code);
+		}
+	} catch (fontParseError) {
+		console.log("*** There was a font parsing error.  " + "Did you copy your font's selection.json file " + "into the assets folder of your application and name it " + fontname + ".json?");
+		console.log("*** fontParseError: " + fontParseError);
+	}
+}
+
+var getFontList = function(fontname, size, options) {
+	if ( typeof fontMaps[fontname] == "undefined")
+		initializeFont(fontname);
+	if ( typeof size == "undefined")
+		size = 32;
+	var scrollView = Ti.UI.createScrollView({
+		layout : "vertical",
+		top : size + "dp"
+	});
+	function showCode(e) {
+		var iconname = e.source.text;
+		alert("Usage:\n" + "var iconlib = require(\"icomoonlib\");\n" + "var icon = iconlib.getIcon(\"" + fontname + "\", \"" + iconname + "\", 32);\n" + "\n");
+	};
+	for (var iconname in fontMaps[fontname]) {
+		var label = Ti.UI.createLabel({
+			left : "10dp",
+			height : size + "dp",
+			width : size + "dp",
+			font : {
+				fontFamily : fontname,
+				fontSize : size + "dp"
+			},
+			text : fontMaps[fontname][iconname],
+			textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+			color : "black"
+		});
+		if ( typeof options == "object") {
+			for (var attr in options) {
+				label[attr] = options[attr];
+			}
+		}
+		var title = Ti.UI.createLabel({
+			text : iconname,
+			size : parseInt(size / 2, 10).toString() + "dp",
+			left : "10dp"
+		});
+		var fontRow = Ti.UI.createView({
+			layout : "horizontal",
+			width : Ti.UI.FILL,
+			height : Ti.UI.SIZE
+		});
+		fontRow.addEventListener("click", showCode);
+		fontRow.add(label);
+		fontRow.add(title);
+		scrollView.add(fontRow);
+	}
+	var displayWin = Ti.UI.createWindow({
+		backgroundColor : "white"
+	});
+	var deviceVersion = parseInt(Titanium.Platform.version.split(".")[0], 10);
+	if (OS_IOS && deviceVersion >= 7)
+		displayWin.top = "20dp";
+
+	var btnClose = Titanium.UI.createButton({
+		title : 'Close',
+		left : "10dp"
+	});
+	btnClose.addEventListener('click', function(e) {
+		displayWin.close();
+	});
+	var fontTitle = Ti.UI.createLabel({
+		text : "font: " + fontname,
+		size : parseInt(size / 2, 10).toString() + "dp",
+		left : "10dp"
+	});
+
+	var header = Ti.UI.createView({
+		layout : "horizontal",
+		top : 0,
+		height : size + "dp",
+		width : Ti.UI.FILL,
+		backgroundColor : "white",
+		borderColor : "gray",
+		borderWidth : 1
+	});
+	header.add(btnClose);
+	header.add(fontTitle);
+	displayWin.add(header);
+	displayWin.add(scrollView);
+	displayWin.open();
+};
+
+var getIconAsLabel = function(fontname, iconname, size, options) {
+	if ( typeof fontMaps[fontname] == "undefined")
+		initializeFont(fontname);
+	var label = Ti.UI.createLabel({
+		height : size + "dp",
+		width : size + "dp",
+		font : {
+			fontFamily : fontname,
+			fontSize : size + "dp"
+		},
+		text : ( typeof iconname == "string" ? fontMaps[fontname][iconname] : String.fromCharCode(iconname)),
+		textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+		color : "black"
+	});
+	if ( typeof options == "object") {
+		for (var attr in options) {
+			label[attr] = options[attr];
+		}
+	}
+	return label;
+};
+
+var getIconAsBlob = function(fontname, iconname, size, options) {
+	var view = Ti.UI.createView({
+		height : Ti.UI.SIZE,
+		width : Ti.UI.SIZE
+	});
+	view.add(getIconAsLabel(fontname, iconname, size, options));
+	return view.toImage(null, true);
+};
+
+var getIconAsImageView = function(fontname, iconname, size, options) {
+	var view = Ti.UI.createView({
+		height : Ti.UI.SIZE,
+		width : Ti.UI.SIZE
+	});
+	view.add(getIconAsLabel(fontname, iconname, size, options));
+	var imageView = Ti.UI.createImageView({
+		image : getIcon(fontname, iconname, size, options) //view.toImage(null,true)
+	});
+	return imageView;
+};
+
+var getIcon = function(fontname, iconname, size, options) {
+	var filename = Ti.Utils.md5HexDigest(fontname + "." + iconname + "." + size + ( typeof options == "object" ? JSON.stringify(options) : "")) + ".png";
+	//console.log("icon filename:" + filename);
+
+	var path = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory, filename);
+	if (path.exists()) {
+		//console.log("*** returning cached version");
+		return path.nativePath;
+	} else {
+		//console.log("*** creating new file: " + path.nativePath);
+		var blob = getIconAsBlob(fontname, iconname, size, options);
+		console.log(blob.apiName);
+		if (Ti.Android) {
+			// this is a workaround for Android because toImage() does not return a blob on Android
+			// https://jira.appcelerator.org/browse/TIMOB-3268
+			// Android toImage() returns a dictionary (with width, height, x, y, cropRect and media)
+			path.write(blob.media);
+		} else {
+			path.write(blob);
+		}
+		return path.nativePath;
+	}
+};
+
+exports.getIcon = getIcon;
+exports.getIconAsLabel = getIconAsLabel;
+exports.getIconAsBlob = getIconAsBlob;
+exports.getIconAsImageView = getIconAsImageView;
+exports.getFontList = getFontList;
